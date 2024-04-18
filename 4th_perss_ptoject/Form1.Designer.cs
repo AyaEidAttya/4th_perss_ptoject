123cec9a19611833615d96a0e29630daefd7b6fb
kasem <youremail@yourdomain.com>
2024-04-19 00:15:35 +0200

    Add project files.

---
+++ 4th_perss_ptoject/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace _4th_perss_ptoject
+{
+    partial class pressure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // pressure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1329, 711);
+            this.Name = "pressure";
+            this.Text = "pressure";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ 4th_perss_ptoject/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4th_perss_ptoject
+{
+    public partial class pressure : Form
+    {
+        public pressure()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            // Panel1
+            Panel panel1 = new Panel();
+            panel1.Dock = DockStyle.Right;
+            panel1.Width = (int)(this.Width * 0.25); // 25% of the form's width
+            this.Controls.Add(panel1);
+            panel1.BackColor = Color.Red;
+
+            // Panel2
+            Panel panel2 = new Panel();
+            panel2.Dock = DockStyle.Left;
+            panel2.Width = (int)(this.Width * 0.75); // 25% of the form's width
+            this.Controls.Add(panel2);
+            panel2.BackColor = Color.Black;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
